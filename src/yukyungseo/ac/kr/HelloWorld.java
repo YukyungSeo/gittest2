2b4f8b2aec86c91acc0ddf56668bfa6c68580fb9
YukyungSeo <syk332@naver.com>
2022-03-07 14:33:48 +0900

    Upload HelloWorld.java
    Upload HelloWorld.java

--- src/yukyungseo/ac/kr/HelloWorld.java
+++ src/yukyungseo/ac/kr/HelloWorld.java
@@ -2,6 +2,7 @@ package yukyungseo.ac.kr;
 
 public class HelloWorld {
 	public static void main (String args[]) {
-		System.out.println("�ǽ�2");
+		System.out.println("½Ç½À2");
+		System.out.println("½Ç½À2");
 	}
 }
